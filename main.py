866f790d4d32fc7159350b91191a9f17b2cccd94
Tess <tess.buchet-le-bihan@esme.fr>
2025-03-11 22:20:14 +0100

    Ajout de la fonctionnalité de choix du type de blague

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pyjokes
 
-def tell_joke(lang="en"):
-    print(pyjokes.get_joke(language=lang))
+def tell_joke(lang="en", category="neutral"):
+    print(pyjokes.get_joke(language=lang, category=category))
 
 if __name__ == "__main__":
     langue = input("Choisissez une langue (en, de, es, it, gr): ")
-    tell_joke(lang)
+    categorie = input("Choisissez une catégorie de blague (neutral, chuck, all): ")
+    tell_joke(langue, categorie)

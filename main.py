da22e1cdf4151e7551382d427aaa6fbf9f539da7
Tess <tess.buchet-le-bihan@esme.fr>
2025-03-12 09:06:32 +0100

    Modification du texte d'invite de langue

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pyjokes
 
-def tell_joke(lang="en"):
-    print(pyjokes.get_joke(language=lang))
-
+def tell_joke(lang="en", category="neutral"):
+    print(pyjokes.get_joke(language=lang, category=category))
+ 
 if __name__ == "__main__":
-    langue = input("Choisissez une langue (en, de, es, it, gr): ")
-    tell_joke(lang)
+    langue = input("Sélectionnez une langue parmi (en, de, es, it, fr) : ")
+    categorie = input("Choisissez une catégorie de blague (neutral, chuck, all): ")
+    tell_joke(langue, categorie)
